286d7e48ada22a8a8ff9044d265881585595fdff
StarGazer1995 <gz19951230@outlook.com>
2026-06-20 10:43:02 +0800

    fix(mcp): register startup tools and bump version

--- src/main.py
+++ src/main.py
@@ -3,7 +3,12 @@ import os
 
 from astropy.utils.iers import conf as iers_conf
 
-# Import modules to register tools
+# Import modules to register tools on process startup.
+import src.functions.celestial.impl  # noqa: F401
+import src.functions.metadata.impl  # noqa: F401
+import src.functions.places.impl  # noqa: F401
+import src.functions.time.impl  # noqa: F401
+import src.functions.weather.impl  # noqa: F401
 from src.server_instance import mcp
 
 
